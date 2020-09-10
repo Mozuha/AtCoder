a1f9295ac70375887e4fc3aaf8349d340bc9c08d
Mozuha <hashimx17@juniata.edu>
2020-09-11 02:27:54 +0900

    ABC042

---
+++ ABC/ABC042/A/main.cpp
@@ -0,0 +1,12 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  vector<int> iroha(3);
+  for (int i = 0; i < 3; i++) cin >> iroha[i];
+  cout << (count(iroha.begin(), iroha.end(), 5) == 2 
+  && count(iroha.begin(), iroha.end(), 7) == 1 ? "YES" : "NO") << endl;
+}
---
+++ ABC/ABC042/B/main.cpp
@@ -0,0 +1,16 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int n, l;
+  cin >> n >> l;
+  vector<string> s(n);
+  rep(i, n) cin >> s[i];
+  sort(s.begin(), s.end());
+  string ans;
+  rep(i, n) ans += s[i];
+  cout << ans << endl;
+}
